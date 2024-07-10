30cd3c28365b36b760a7ac9a2da408472c88fbf2
Ashley DANG <dangthaomy13062013@gmail.com>
2024-07-10 11:01:36 +1000

    finish signup signin

--- config/site.ts
+++ config/site.ts
@@ -1,4 +1,4 @@
 export const siteConfig = {
-    name: "Taskify",
+    name: "Trello",
     description: "Collaborate, manage projects, and reach new productivity peaks. From high rises to the home office, the way your team works is unique—accomplish it all with Taskify.",
 }
