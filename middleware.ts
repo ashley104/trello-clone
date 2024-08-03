54f3a943c58eb62dd97580f8a694e76decbd2527
Ashley Dang <dangthaomy13062013@gmail.com>
2024-08-04 08:30:16 +1000

    add demo sidebar

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,36 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+import { clerkClient } from "@clerk/clerk-sdk-node";
 
-export default clerkMiddleware();
+export default clerkMiddleware(async (auth, req) => {
+  const isPublicRoute = createRouteMatcher(["/"]);
+
+  const isProtectedRoute = createRouteMatcher([
+    `/organization/${auth().orgId}`,
+  ]);
+
+  if(auth().userId && isPublicRoute(req)) {
+    let path = "";
+
+    if (auth().orgId) {
+      path = `/organization/${auth().orgId}`;
+    } else {
+      // Create an organization if the user does not have one
+      const organization = await clerkClient.organizations.createOrganization({
+        name: "Trello Workspace",
+        createdBy: auth().userId ?? "",
+      });
+      path = `/organization/${organization.id}`;
+    }
+
+    const organisationPageUrl = new URL(path, req.url);
+    return NextResponse.redirect(organisationPageUrl);
+  }
+
+  if (!auth().userId && isProtectedRoute(req)) {
+    return auth().redirectToSignIn();
+  }
+});
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
