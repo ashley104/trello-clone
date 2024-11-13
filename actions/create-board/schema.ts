0f53244dd1db7872d966e9064ec8fae9a691048e
Ashley Dang <dangthaomy13062013@gmail.com>
2024-11-13 18:09:53 +1100

    add server action

---
+++ actions/create-board/schema.ts
@@ -0,0 +1,8 @@
+import { z } from "zod";
+
+export const CreateBoard = z.object({
+  title: z.string({
+    required_error: "Title is required",
+    invalid_type_error: "Title is required",
+  }).min(3, "Title must be at least 3 characters."),
+})
--- middleware.ts
+++ middleware.ts
@@ -2,35 +2,44 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 import { clerkClient } from "@clerk/clerk-sdk-node";
 
-export default clerkMiddleware(async (auth, req) => {
-  const isPublicRoute = createRouteMatcher(["/"]);
+const isProtectedRoute = createRouteMatcher(['/organization(.*)'])
+const isPublicRoute = createRouteMatcher(['/'])
 
-  if(auth().userId && isPublicRoute(req)) {
-    let path = "";
+export default clerkMiddleware(async (auth, req) => {
+  if (isProtectedRoute(req)) {
+    auth().protect();
+  }
+  if (auth().userId && isPublicRoute(req)) {
+    let path = "/select-org";
 
     if (auth().orgId) {
       path = `/organization/${auth().orgId}`;
     } else {
-      // Create an organization if the user does not have one
-      const organization = await clerkClient.organizations.createOrganization({
-        name: "Trello Workspace",
-        createdBy: auth().userId ?? "",
-      });
-      path = `/organization/${organization.id}`;
+      // Fetch the user's organizations
+      const userId = auth().userId;
+      if (!userId) {
+        throw new Error("User ID is null");
+      }
+      const user = await clerkClient.users.getUser(userId);
+      const organizations: any[] = Array.isArray(user.publicMetadata.organizationMemberships) ? user.publicMetadata.organizationMemberships : [];
+
+      if (organizations.length > 0) {
+        // If the user has organizations, redirect to the first one
+        path = `/organization/${organizations[0].organization.id}`;
+      } else {
+        // Create an organization if the user does not have one
+        const organization = await clerkClient.organizations.createOrganization({
+          name: "Trello Workspace",
+          createdBy: auth().userId ?? "",
+        });
+        path = `/organization/${organization.id}`;
+      }
     }
 
     const organisationPageUrl = new URL(path, req.url);
     return NextResponse.redirect(organisationPageUrl);
   }
-
-  const isProtectedRoute = createRouteMatcher([
-    `/organization/${auth().orgId}(.*)`,
-  ]);
-
-  if (!auth().userId && !isPublicRoute(req)) {
-    return auth().redirectToSignIn();
-  }
-});
+})
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
