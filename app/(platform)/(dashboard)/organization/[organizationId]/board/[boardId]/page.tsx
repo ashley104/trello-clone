92f1953c44ebd34b9e62e5ddd308a323adcc3004
Ashley Dang <dangthaomy13062013@gmail.com>
2024-11-17 18:51:26 +1100

    add list board

---
+++ app/(platform)/(dashboard)/organization/[organizationId]/board/[boardId]/page.tsx
@@ -0,0 +1,46 @@
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
+interface BoardIdPageProps {
+  params: {
+    boardId: string;
+  };
+};
+
+const BoardIdPage = async ({
+  params,
+}: BoardIdPageProps) => {
+  const { orgId } = auth();
+
+  if (!orgId) {
+    redirect("/select-org");
+  }
+
+  const lists = await db.list.findMany({
+    where: {
+      boardId: params.boardId,
+      board: {
+        orgId,
+      }
+    },
+    include: {
+      cards: {
+        orderBy: {
+          order: "asc",
+        },
+      },
+    },
+    orderBy: {
+      order: "asc",
+    }
+  });
+
+  return (
+    <div>
+      Board ID!
+    </div>
+  )
+}
+
+export default BoardIdPage;
