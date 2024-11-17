71db7bb370249476b854c9f45ffd20864aded31b
Ashley Dang <dangthaomy13062013@gmail.com>
2024-11-17 21:36:44 +1100

    add header

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
@@ -2,6 +2,8 @@
 
 import { ListWithCards } from "@/types";
 import { ListForm } from "./list-form";
+import { use, useEffect, useState } from "react";
+import { ListItem } from "./list-item";
 
 interface ListContainerProps {
   data: ListWithCards[];
@@ -12,8 +14,17 @@ export const ListContainer = ({
   data,
   boardId,
 }: ListContainerProps) => {
+  const [orderData, setOrderData] = useState(data);
+
+  useEffect(() => {
+    setOrderData(data);
+  }, [data]);
+
   return (
-    <ol>
+    <ol className="flex gap-x-3 h-full">
+      {orderData.map((list, index) => (
+        <ListItem key={list.id} data={list} index={index} />
+      ))}
       <ListForm />
       <div className="flex-shrink-0 w-1" />
     </ol> 
