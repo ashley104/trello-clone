92a580e1d4ebfc401c973bb27c34b69acfda0874
Ashley Dang <dangthaomy13062013@gmail.com>
2024-11-19 18:24:02 +1100

    add edit card title

--- app/(platform)/layout.tsx
+++ app/(platform)/layout.tsx
@@ -1,4 +1,7 @@
+import { ModalProvider } from '@/components/providers/modal-provider';
+import { QueryProvider } from '@/components/providers/query-provider';
 import { ClerkProvider } from '@clerk/nextjs';
+import { Query } from '@tanstack/react-query';
 import { Toaster } from "sonner";
 
 const PlatformLayout = ({ 
@@ -8,8 +11,11 @@ const PlatformLayout = ({
 }) => {
     return (
         <ClerkProvider>
-            <Toaster />
-            {children}
+            <QueryProvider>
+                <Toaster />
+                <ModalProvider />
+                {children}
+            </QueryProvider>
         </ClerkProvider>
     );
 };
